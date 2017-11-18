ae0f8c6921378a2a562dc98925b6a34a9c5746fa
Maciej Lobodzinski <matt@prismake.com>
2017-11-17 21:55:25 -0600

    Add Product Cell Class.

---
+++ Northwind/Northwind/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  Northwind
+//
+//  Created by Maciej Łobodziński on 12/12/2017.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
