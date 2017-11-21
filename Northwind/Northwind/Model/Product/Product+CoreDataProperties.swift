d5d8db7dee41b83e999e4a10e8e939ebf9908b5a
Maciej Lobodzinski <matt@prismake.com>
2017-11-20 23:53:45 -0600

    Add Product class to model.

---
+++ Northwind/Northwind/Model/Product/Product+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Product+CoreDataProperties.swift
+//  Northwind
+//
+//  Created by Maciej Łobodziński on 13/12/2017.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Product {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Product> {
+        return NSFetchRequest<Product>(entityName: "Product")
+    }
+
+    @NSManaged public var discontinued: Bool
+    @NSManaged public var productName: String?
+    @NSManaged public var quantityPerUnit: Int16
+    @NSManaged public var reorderLevel: Int16
+    @NSManaged public var unitPrice: NSDecimalNumber?
+    @NSManaged public var unitsInStock: Int16
+    @NSManaged public var unitsOnOrder: Int16
+    @NSManaged public var category: Category?
+    @NSManaged public var supplier: Supplier?
+
+}
---
+++ Northwind/Northwind/Model/Product/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  Northwind
+//
+//  Created by Maciej Łobodziński on 13/12/2017.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {
+
+}
