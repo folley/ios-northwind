7ca6a4b8842b4e5bf9d240c4fa33f838715f9960
Corezeone <s.korzeniak@gmail.com>
2018-01-13 13:17:55 +0100

    add Single RelationshipTableViewCell

---
+++ Northwind/Northwind/Views/Cells/SingleRelationshipTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SingleRelationshipTableViewCell.swift
+//  Northwind
+//
+//  Created by Sebastian Korzeniak
+//
+
+import UIKit
+
+class SingleRelationshipTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
