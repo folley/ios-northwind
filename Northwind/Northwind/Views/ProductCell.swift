0d35e6964ae57774a6b7a40bac0ff15100e6ebd7
Maciej Lobodzinski <matt@prismake.com>
2018-01-19 21:51:19 +0100

    Polishing Product cell view.

--- Northwind/Northwind/Views/ProductCell.swift
+++ Northwind/Northwind/Views/ProductCell.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ProductCell: UITableViewCell {
 
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var descLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        cardView.layer.cornerRadius = 5
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        cardView.layer.shadowOpacity = 0.2
+        cardView.layer.shadowRadius = 5.0
+        cardView.layer.masksToBounds =  false
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
